ff2043e0826db2bdebb79cf8c7e90112d5f49038
Alan Xavier Martial <52906032+alanxm03@users.noreply.github.com>
2023-08-06 13:57:59 +0530

    routes

--- index.js
+++ index.js
@@ -21,26 +21,26 @@ app.use(cors());
 app.use(express.static("public"));
 //index
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,"public/views/index.html"))
+    res.sendFile(path.join(__dirname,"/index.html"))
 })
 
 //party
 app.get('/party',(req,res)=>{
-    res.sendFile(path.join(__dirname,"public/views/party.html"))
+    res.sendFile(path.join(__dirname,"/party.html"))
 })
 
 //leader
 app.get('/leader',(req,res)=>{
-    res.sendFile(path.join(__dirname,"public/views/leader.html"))
+    res.sendFile(path.join(__dirname,"/leader.html"))
 })
 
 //download leader image
 app.get('/pathiverakam',(req,res)=>{
-    res.sendFile(path.join(__dirname,"public/views/download.html"))
+    res.sendFile(path.join(__dirname,"/download.html"))
 })
 
 app.get('/contact',(req,res)=>{
-    res.sendFile(path.join(__dirname,"public/views/contact.html"))
+    res.sendFile(path.join(__dirname,"/contact.html"))
 })
 
 //memberreg
